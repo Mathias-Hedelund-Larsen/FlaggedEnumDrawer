72f20669e34b7839a4b70787ade777532649259e
MathDodo <mathias.h.larsen@jp.dk>
2020-03-18 13:31:44 +0100

    Only needs a readme

---
+++ Assets/HephaestusForge/Editor/FlaggedEnum/EnumPropertyDrawer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace HephaestusForge.FlaggedEnum
+{
+    [CustomPropertyDrawer(typeof(Enum), true)]
+    public class EnumPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (fieldInfo.FieldType.IsDefined(typeof(FlagsAttribute), false))
+            {
+                var enumVal = (Enum)Enum.ToObject(fieldInfo.FieldType, property.intValue);
+
+                property.intValue = (int)Enum.ToObject(fieldInfo.FieldType, EditorGUI.EnumFlagsField(position, label, enumVal));
+            }
+            else
+            {
+                EditorGUI.PropertyField(position, property);
+            }
+        }
+    }
+}
